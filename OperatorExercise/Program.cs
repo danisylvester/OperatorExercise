ac6bc1b04154981c8389daa66f6b051289e6c7eb
danisylvester <maria.sylvester10@gmail.com>
2020-08-24 12:47:42 -0500

    conditional print line of quotient & remainder / calculated area of circle

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4) 
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+
+            Console.WriteLine("...................................................");
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+            Console.WriteLine("...................................................");
+
+            int i = 3;
+            int j = 4;
+            int k = ++i * j++;
+            Console.WriteLine(k);
+        }
+
+        public static double AreaOfCircle(double r)
+        {
+            return Math.PI * Math.Pow(r,2);
+        }
+
+    }
+}
